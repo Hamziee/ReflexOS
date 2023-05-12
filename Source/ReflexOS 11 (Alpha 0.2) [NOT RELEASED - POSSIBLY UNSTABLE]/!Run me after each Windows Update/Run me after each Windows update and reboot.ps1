94a328213db10d8bd0e4c203b16bc3a8b56c162b
Hamziee <91210085+Hamziee@users.noreply.github.com>
2023-05-12 16:12:42 +0200

    Working on ReflexOS

--- Source/ReflexOS 11 (Alpha 0.2) [NOT RELEASED - POSSIBLY UNSTABLE]/!Run me after each Windows Update/Run me after each Windows update and reboot.ps1
+++ Source/ReflexOS 11 (Alpha 0.2) [NOT RELEASED - POSSIBLY UNSTABLE]/!Run me after each Windows Update/Run me after each Windows update and reboot.ps1
@@ -1,8 +1,46 @@
-# Ask for elevated permissions if required
-If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
-	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
-	Exit
+echo " "
+echo " _____    ______   ______   _        ______  __   __   ____     _____ "
+echo "|  __ \  |  ____| |  ____| | |      |  ____| \ \ / /  / __ \   / ____|"
+echo "| |__) | | |__    | |__    | |      | |__     \ V /  | |  | | | (___  "
+echo "|  _  /  |  __|   |  __|   | |      |  __|     > <   | |  | |  \___ \ "
+echo "| | \ \  | |____  | |      | |____  | |____   / . \  | |__| |  ____) |"
+echo "|_|  \_\ |______| |_|      |______| |______| /_/ \_\  \____/  |_____/ "
+echo " "
+echo " "
+echo "             _      _____  _    _             ___        ___  "
+echo "       /\   | |    |  __ \| |  | |   /\      / _ \      |__ \ "
+echo "      /  \  | |    | |__) | |__| |  /  \    | | | |        ) |"
+echo "     / /\ \ | |    |  ___/|  __  | / /\ \   | | | |       / / "
+echo "    / ____ \| |____| |    | |  | |/ ____ \  | |_| |  _   / /_ "
+echo "   /_/    \_\______|_|    |_|  |_/_/    \_\  \___/  (_) |____|"
+echo " "
+echo "Windows Update Script Credits:"
+echo "ReflexOS By Hamziee"
+echo "The Ultimate Windows Utility by Chris Titus Tech"
+echo "BloatyNosy by builtbybel"
+echo " "
+echo "Windows Update script will start in 10 seconds, close now if you want to cancel."
+Start-Sleep 10
+echo "Starting..."
+Start-Sleep 2
+echo " "
+echo " "
+#This will self elevate the script so with a UAC prompt since this script needs to be run as an Administrator in order to function properly.
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+    Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue."
+    Start-Sleep 1
+    Write-Host "                                               3"
+    Start-Sleep 1
+    Write-Host "                                               2"
+    Start-Sleep 1
+    Write-Host "                                               1"
+    Start-Sleep 1
+    Start-Process powershell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+    Exit
 }
+
+#no errors throughout
+$ErrorActionPreference = 'silentlycontinue'
 
 Rename-Computer -NewName "ReflexOS" -Force
 
@@ -67,6 +105,8 @@ Copy-Item "C:\ReflexOS 10 (Alpha 0.1)\icons\User Account Pictures\*" "C:\Program
 'sc stop "wsearch" && sc config "wsearch" start=disabled' | cmd
 
 iwr -useb https://christitus.com/win | iex
+
+.\"C:\ReflexOS 11 (Alpha 0.2)\files\BloatyNosyApp 0.70.149.exe"
 
 Write-Output "Press any key to reboot now"
 cmd /c 'pause>nul'
