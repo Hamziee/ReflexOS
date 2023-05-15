4bab6dd73f6d93d921b201999a06bc6505cd6623
Hamza <91210085+Hamziee@users.noreply.github.com>
2023-05-15 09:59:10 +0200

    Working on ReflexOS

--- Source/ReflexOS 10 (Alpha 0.2) [NOT RELEASED - POSSIBLY UNSTABLE]/files/update.ps1
+++ Source/ReflexOS 10 (Alpha 0.2) [NOT RELEASED - POSSIBLY UNSTABLE]/files/update.ps1
@@ -1,58 +1,16 @@
-echo " "
-echo " _____    ______   ______   _        ______  __   __   ____     _____ "
-echo "|  __ \  |  ____| |  ____| | |      |  ____| \ \ / /  / __ \   / ____|"
-echo "| |__) | | |__    | |__    | |      | |__     \ V /  | |  | | | (___  "
-echo "|  _  /  |  __|   |  __|   | |      |  __|     > <   | |  | |  \___ \ "
-echo "| | \ \  | |____  | |      | |____  | |____   / . \  | |__| |  ____) |"
-echo "|_|  \_\ |______| |_|      |______| |______| /_/ \_\  \____/  |_____/ "
-echo " "
-echo " "
-echo "             _      _____  _    _             ___        ___  "
-echo "       /\   | |    |  __ \| |  | |   /\      / _ \      |__ \ "
-echo "      /  \  | |    | |__) | |__| |  /  \    | | | |        ) |"
-echo "     / /\ \ | |    |  ___/|  __  | / /\ \   | | | |       / / "
-echo "    / ____ \| |____| |    | |  | |/ ____ \  | |_| |  _   / /_ "
-echo "   /_/    \_\______|_|    |_|  |_/_/    \_\  \___/  (_) |____|"
-echo " "
-echo "Install Script Credits:"
-echo "ReflexOS By Hamziee"
-echo "Windows10Debloater by Sycnex"
-echo "Win10 Initial Setup Script by Disassembler"
-echo "The Ultimate Windows Utility by Chris Titus Tech"
-echo " "
-echo "Install script will start in 10 seconds, close now if you want to cancel."
-Start-Sleep 10
-echo "Starting..."
-Start-Sleep 2
-echo " "
-echo " "
-#This will self elevate the script so with a UAC prompt since this script needs to be run as an Administrator in order to function properly.
-If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
-    Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue."
-    Start-Sleep 1
-    Write-Host "                                               3"
-    Start-Sleep 1
-    Write-Host "                                               2"
-    Start-Sleep 1
-    Write-Host "                                               1"
-    Start-Sleep 1
-    Start-Process powershell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
-    Exit
-}
-
 #no errors throughout
 $ErrorActionPreference = 'silentlycontinue'
 
-Rename-Computer -NewName "ReflexOS" -Force
+Rename-Computer -NewName "ReflexOS" -confirm:$false -Force
 
-Install-Module -Name WindowsOEMinformation
+Install-Module -Name WindowsOEMinformation -confirm:$false -Force
 
 $oemInfo = @{
     Manufacturer = 'ReflexOS'
     Model = 'ReflexOS 10 (Alpha 0.2)'
     SupportUrl = 'http://reflexos.heo-systems.net/'
 }
-Set-WindowsOemInformation @oemInfo
+Set-WindowsOemInformation @oemInfo -confirm:$false -Force
 
 $setwallpapersrc = @"
 using System.Runtime.InteropServices;
@@ -107,6 +65,5 @@ Copy-Item "C:\ReflexOS 10 (Alpha 0.2)\icons\User Account Pictures\*" "C:\Program
 
 iwr -useb https://christitus.com/win | iex
 
-Write-Output "Press any key to reboot now"
-cmd /c 'pause>nul'
-Restart-Computer
+& 'C:\ReflexOS 10 (Alpha 0.2)\files\BloatyNosy 0.70.149.exe'
+
